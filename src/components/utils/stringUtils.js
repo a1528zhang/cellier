1280e072da150641228147a704ba32384ab5c3b0
wushuang <beingswu@gmail.com>
2016-08-21 01:03:55 +0800

    webpack done

--- src/components/utils/stringUtils.js
+++ src/components/utils/stringUtils.js
@@ -1,5 +1,5 @@
 export function cutUrl(originalUrl){
-    let start = originalUrl.indexOf("/#/")+3;
+    let start = originalUrl.indexOf("#/")+2;
     let end = originalUrl.indexOf("?");
     let startsententce = originalUrl.substring(start,end);
     return startsententce;
